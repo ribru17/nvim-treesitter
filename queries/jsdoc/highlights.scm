c6305c6eb72eab4e2121ed39e01e8bf8235ebf98
Riley Bruins <ribru17@hotmail.com>
2024-09-23 17:32:02 -0700

    fix(jsdoc): recognize optional identifiers in tags
    This commit also narrows the scope of the `"/"` highlight as it would
    override the comment highlight for the final slash in, e.g., `/**  */`.

--- queries/jsdoc/highlights.scm
+++ queries/jsdoc/highlights.scm
@@ -11,30 +11,46 @@
 
 [
   ":"
-  "/"
   "."
   "#"
   "~"
 ] @punctuation.delimiter
 
-(identifier) @variable
+(path_expression
+  "/" @punctuation.delimiter)
 
 (tag
   (tag_name) @_name
-  (identifier) @function
+  [
+    (identifier) @function
+    (optional_identifier
+      (identifier) @function)
+  ]
   (#any-of? @_name "@callback" "@function" "@func" "@method"))
 
 (tag
   (tag_name) @_name
-  (identifier) @variable.parameter
+  [
+    (identifier) @variable.parameter
+    (optional_identifier
+      (identifier) @variable.parameter)
+  ]
   (#any-of? @_name "@param" "@arg" "@argument"))
 
 (tag
   (tag_name) @_name
-  (identifier) @property
+  [
+    (identifier) @property
+    (optional_identifier
+      (identifier) @property)
+  ]
   (#any-of? @_name "@prop" "@property"))
 
 (tag
   (tag_name) @_name
-  (identifier) @type
+  [
+    (identifier) @type
+    (optional_identifier
+      (identifier) @type)
+  ]
   (#eq? @_name "@typedef"))
